c08a0e1ff154dde231e85b42092912003430beee
Hordesk <mathias.faure.31@gmail.com>
2015-01-21 13:58:37 +0100

    Update FormActivity.java

--- app/src/main/java/fr/m2dl/miniprojet/FormActivity.java
+++ app/src/main/java/fr/m2dl/miniprojet/FormActivity.java
@@ -1,9 +1,13 @@
 package fr.m2dl.miniprojet;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import org.w3c.dom.Document;
@@ -15,6 +19,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.m2dl.miniprojet.domain.Detail;
 import fr.m2dl.miniprojet.domain.Type;
 
 /**
@@ -22,61 +27,72 @@ import fr.m2dl.miniprojet.domain.Type;
  */
 public class FormActivity extends Activity {
 
+    private Context context;
     private Spinner spinnerType;
+    private List<Type> typeList = null;
+    private List<Detail> detailList = null;
     private Spinner spinnerDetails;
+    private LinearLayout formLayout;
     private String NODE_EMP = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        this.context = this;
 
         spinnerType = (Spinner) findViewById(R.id.spinnerType);
-        spinnerDetails = (Spinner) findViewById(R.id.spinnerDetail);
-
+        formLayout = (LinearLayout) findViewById(R.id.formLayout);
 
         List<String> list = new ArrayList<String>();
-        list.add("Animal");
-        list.add("Végétal");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerType.setAdapter(dataAdapter);
 
 
         try {
             AssetManager manager = getAssets();
             InputStream stream;
             stream = manager.open("cleCarac.xml");
-            List<Type> typeList = SAXXMLParser.parse(stream);
+            typeList = SAXXMLParser.parse(stream);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-//        XMLDomParser parser = new XMLDomParser();
-//        AssetManager manager = getAssets();
-//        InputStream stream;
-//        try {
-//            stream = manager.open("employee.xml");
-//            Document doc = parser.getDocument(stream);
-//
-//            // Get elements by name employee
-//            NodeList nodeList = doc.getElementsByTagName(NODE_EMP);
-//
-//            /*
-//             * for each <employee> element get text of name, salary and
-//             * designation
-//             */
-//            // Here, we have only one <employee> element
-//            for (int i = 0; i < nodeList.getLength(); i++) {
-//                Element e = (Element) nodeList.item(i);
-//                nameText.setText(parser.getValue(e, NODE_NAME));
-//                salaryText.setText(parser.getValue(e, NODE_SALARY));
-//                designationText.setText(parser.getValue(e, NODE_DESIGNATION));
-//            }
-//        } catch (IOException e1) {
-//            e1.printStackTrace();
-//        }
+
+        for(Type t : typeList){
+            list.add(t.toString());
+
+
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        spinnerType.setAdapter(dataAdapter);
+
+
+
+        spinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Type t = typeList.get(position);
+                Spinner s = null;
+                if(t.getDetailList().size()>0){
+                    s = new Spinner(context);
+                    formLayout.addView(s);
+                }
+
+                detailList = t.getDetailList();
+                List<String> list = new ArrayList<String>();
+                for (Detail d : detailList){
+                    list.add(d.toString());
+                }
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(context,
+                        android.R.layout.simple_spinner_item, list);
+                s.setAdapter(dataAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
